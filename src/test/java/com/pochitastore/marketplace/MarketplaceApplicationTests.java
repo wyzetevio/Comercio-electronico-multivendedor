cea5a0f81c452475a8d80bef72a59281ebc5f275
Juan Colqui <colquijuan51@gmail.com>
2026-05-15 17:51:19 -0500

    feat: estructura base del backend con capas y dependencias

---
+++ src/test/java/com/pochitastore/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pochitastore.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
